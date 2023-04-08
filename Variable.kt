f329a508b9f64fb431f75c07bab8796fa897e096
ArijitRoy22 <2002arijit22@gmail.com>
2023-04-08 13:47:57 +0530

    try

---
+++ Variable.kt
@@ -0,0 +1,16 @@
+fun distance() {
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double =  trip1 + trip2 + trip3
+    println("$totalTripLength miles left to destination")
+}
+
+fun main() {
+    var cartTotal = 0
+    println("Total: $cartTotal")
+
+    cartTotal = 20
+    println("Total: $cartTotal")
+    distance()
+}
